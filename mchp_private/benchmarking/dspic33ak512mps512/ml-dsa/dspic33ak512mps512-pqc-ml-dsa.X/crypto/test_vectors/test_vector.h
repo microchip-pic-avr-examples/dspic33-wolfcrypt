e71f2b1e1afdb6444e3071b30ff07e051917818a
C71887 <Gianni.Consiglio@microchip.com>
2026-01-15 15:48:41 -0700

    M16CRYPTO-200 update benchmarking project

---
+++ mchp_private/benchmarking/dspic33ak512mps512/ml-dsa/dspic33ak512mps512-pqc-ml-dsa.X/crypto/test_vectors/test_vector.h
@@ -0,0 +1,46 @@
+/*
+© [2025] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+#ifndef TEST_VECTOR_H
+#define	TEST_VECTOR_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include "wolfssl/wolfcrypt/dilithium.h"
+    
+typedef struct
+{
+    const byte* publicKey;
+    const word32 publicKeyLength;
+	const byte* signature;
+    const word32 signatureSize;
+	const byte* message;
+    const word32 messageSize;
+} ML_DSA_SIG_VER_TEST_VECTOR;
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TEST_VECTOR_H */
