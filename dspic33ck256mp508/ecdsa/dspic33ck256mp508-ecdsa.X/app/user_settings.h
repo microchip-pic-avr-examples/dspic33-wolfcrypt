ecf2b0637f1aec270ab4e9517f57e259fe3f3b63
C71887 <Gianni.Consiglio@microchip.com>
2026-01-16 14:40:14 -0700

    M16CRYPTO-199 Add ECDSA wolfCrypt demo for dsPIC33CK

---
+++ dspic33ck256mp508/ecdsa/dspic33ck256mp508-ecdsa.X/app/user_settings.h
@@ -0,0 +1,74 @@
+/*
+© [2025] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip
+    software and any derivatives exclusively with Microchip products.
+    You are responsible for complying with 3rd party license terms
+    applicable to your use of 3rd party software (including open source
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.?
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR
+    THIS SOFTWARE.
+*/
+
+#ifndef USER_SETTINGS_H
+#define USER_SETTINGS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    // wolfCrypt Configuration Options
+    #define NO_MD4                    // Disable MD4 related APIs
+    #define NO_MD5                    // Disable MD5 related APIs
+    #define NO_SHA                    // Disable SHA1 related APIs
+    #define NO_DSA                    // Disable DSA related APIs
+    #define NO_RSA                    // Disable RSA related APIs
+    #define NO_DH                     // Disable DH related APIs
+    #define NO_AES                    // Disable AES related APIs
+    #define NO_ASN                    // Disable ASN related APIs
+    #define NO_DES3                   // Disable DES3 related APIs
+    #define NO_64BIT                  // Disable 64-bit data types
+    #define NO_FILESYSTEM             // Specify device does not use a filesystem
+    #define WOLFSSL_NO_MALLOC         // Disable usage of dynamic memory allocation
+    #define WOLFSSL_OLD_OID_SUM       // Use smaller OID sum values for 16-bit device
+    #define WOLFSSL_IGNORE_FILE_WARN  // Disable warning regarding inlined misc.c functions
+    #define WOLFCRYPT_ONLY            // Specify using wolfCrypt APIs only
+    #define WOLF_C89                  // Specify strcmp function types
+    #define WOLF_CRYPTO_CB            // Enable cryptocb functions for use by ECC APIs
+
+
+    // wolfCrypt Random Configuration Options
+    #define NO_DEV_RANDOM             // Disables the inclusion of wolfCrypt's RNG header file (verify APIs do not need it)
+
+    // wolfCrypt SP Configuration Options
+    #define WOLFSSL_SP_SMALL          // Reduce SP library code size
+    #define WOLFSSL_SP_LOW_MEM        // Reduce memory usage of SP algorithms
+    #define WOLFSSL_SP_NO_DYN_STACK   // Dynamic arrays instead of dynamic stack usage
+
+    // wolfCrypt ECDSA Configuration Options
+    #define HAVE_ECC_VERIFY           // Enable ECC verify
+    #define HAVE_ECC                  // Enable ECC curves
+    #define HAVE_ECC384               // Ensures ECC P384 is available
+    #define ECC_TIMING_RESISTANT      // Required for ECC operations
+
+    // dsPIC33C Support
+    #ifdef __dsPIC33C__
+        #define WC_16BIT_CPU          // Specify 16-bit integer sizes
+        #define SIZEOF_LONG_LONG 4    // Define long value as 4 bytes
+        #define SINGLE_THREADED       // Specify device is single threaded
+    #endif
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif /* USER_SETTINGS_H */
---
+++ dspic33ck256mp508/ecdsa/dspic33ck256mp508-ecdsa.X/mcc_generated_files/system/pins.h
@@ -0,0 +1,107 @@
+/**
+ * PINS Generated Driver Header File 
+ * 
+ * @file      pins.h
+ *            
+ * @defgroup  pinsdriver Pins Driver
+ *            
+ * @brief     The Pin Driver directs the operation and function of 
+ *            the selected device pins using dsPIC MCUs.
+ *
+ * @skipline @version   Firmware Driver Version 1.0.2
+ *
+ * @skipline @version   PLIB Version 1.4.0
+ *
+ * @skipline  Device : dsPIC33CK256MP508
+*/
+
+/*
+© [2025] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+#ifndef PINS_H
+#define PINS_H
+// Section: Includes
+#include <xc.h>
+
+// Section: Device Pin Macros
+
+/**
+ * @ingroup  pinsdriver
+ * @brief    Sets the RE6 GPIO Pin which has a custom name of BENCHMARKING_RE6 to High
+ * @pre      The RE6 must be set as Output Pin             
+ * @param    none
+ * @return   none  
+ */
+#define BENCHMARKING_RE6_SetHigh()          (_LATE6 = 1)
+
+/**
+ * @ingroup  pinsdriver
+ * @brief    Sets the RE6 GPIO Pin which has a custom name of BENCHMARKING_RE6 to Low
+ * @pre      The RE6 must be set as Output Pin
+ * @param    none
+ * @return   none  
+ */
+#define BENCHMARKING_RE6_SetLow()           (_LATE6 = 0)
+
+/**
+ * @ingroup  pinsdriver
+ * @brief    Toggles the RE6 GPIO Pin which has a custom name of BENCHMARKING_RE6
+ * @pre      The RE6 must be set as Output Pin
+ * @param    none
+ * @return   none  
+ */
+#define BENCHMARKING_RE6_Toggle()           (_LATE6 ^= 1)
+
+/**
+ * @ingroup  pinsdriver
+ * @brief    Reads the value of the RE6 GPIO Pin which has a custom name of BENCHMARKING_RE6
+ * @param    none
+ * @return   none  
+ */
+#define BENCHMARKING_RE6_GetValue()         _RE6
+
+/**
+ * @ingroup  pinsdriver
+ * @brief    Configures the RE6 GPIO Pin which has a custom name of BENCHMARKING_RE6 as Input
+ * @param    none
+ * @return   none  
+ */
+#define BENCHMARKING_RE6_SetDigitalInput()  (_TRISE6 = 1)
+
+/**
+ * @ingroup  pinsdriver
+ * @brief    Configures the RE6 GPIO Pin which has a custom name of BENCHMARKING_RE6 as Output
+ * @param    none
+ * @return   none  
+ */
+#define BENCHMARKING_RE6_SetDigitalOutput() (_TRISE6 = 0)
+
+/**
+ * @ingroup  pinsdriver
+ * @brief    Initializes the PINS module
+ * @param    none
+ * @return   none  
+ */
+void PINS_Initialize(void);
+
+
+
+#endif
