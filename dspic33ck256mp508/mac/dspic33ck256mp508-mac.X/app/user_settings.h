c03f721d2acad16e35e23b06737d940fe10bf4cb
Noah Richey <Noah.Richey@microchip.com>
2026-01-19 15:19:49 -0700

    M16CRYPTO-185 Getting MCC and existing files in the project

---
+++ dspic33ck256mp508/mac/dspic33ck256mp508-mac.X/app/user_settings.h
@@ -0,0 +1,50 @@
+/*
+? [2025] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip
+    software and any derivatives exclusively with Microchip products.
+    You are responsible for complying with 3rd party license terms
+    applicable to your use of 3rd party software (including open source
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.?
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR
+    THIS SOFTWARE.
+*/
+
+#ifndef USER_SETTINGS_H
+#define USER_SETTINGS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    // wolfCrypt Configuration Options
+    #define WOLFSSL_OLD_OID_SUM       // Use smaller OID sum values for 16-bit device
+    #define NO_FILESYSTEM             // Specify device does not use a filesystem
+    #define WOLFSSL_IGNORE_FILE_WARN  // Disable warning regarding inlined misc.c functions
+    #define WC_NO_RNG                 // Disable RNG related APIs
+    #define WOLFCRYPT_ONLY            // Specify using wolfCrypt APIs only
+
+    // wolfCrypt MAC Configuration Options
+    #define WOLFSSL_CMAC              // Enable AES CMAC APIs
+
+    // dsPIC33C Support
+    #ifdef __dsPIC33C__
+        #define WC_16BIT_CPU          // Specify 16-bit integer sizes
+        #define SIZEOF_LONG_LONG 4    // Define long value as 4 bytes
+        #define SINGLE_THREADED       // Specify device is single threaded
+    #endif
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif /* USER_SETTINGS_H */
---
+++ dspic33ck256mp508/mac/dspic33ck256mp508-mac.X/mcc_generated_files/system/src/pins.c
@@ -0,0 +1,113 @@
+ /**
+ * PINS Generated Driver Source File 
+ * 
+ * @file      pins.c
+ *            
+ * @ingroup   pinsdriver
+ *            
+ * @brief     This is the generated driver source file for PINS driver.
+ *
+ * @skipline @version   Firmware Driver Version 1.0.2
+ *
+ * @skipline @version   PLIB Version 1.4.1
+ *
+ * @skipline  Device : dsPIC33CK256MP508
+*/
+
+/*
+© [2026] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+// Section: Includes
+#include <xc.h>
+#include <stddef.h>
+#include "../pins.h"
+
+// Section: File specific functions
+
+// Section: Driver Interface Function Definitions
+void PINS_Initialize(void)
+{
+    /****************************************************************************
+     * Setting the Output Latch SFR(s)
+     ***************************************************************************/
+    LATA = 0x0000U;
+    LATB = 0x0000U;
+    LATC = 0x0000U;
+    LATD = 0x0008U;
+    LATE = 0x0000U;
+
+    /****************************************************************************
+     * Setting the GPIO Direction SFR(s)
+     ***************************************************************************/
+    TRISA = 0x001FU;
+    TRISB = 0xFFFFU;
+    TRISC = 0xFFFFU;
+    TRISD = 0xFFF7U;
+    TRISE = 0xFFBFU;
+
+
+    /****************************************************************************
+     * Setting the Weak Pull Up and Weak Pull Down SFR(s)
+     ***************************************************************************/
+    CNPUA = 0x0000U;
+    CNPUB = 0x0000U;
+    CNPUC = 0x0000U;
+    CNPUD = 0x0000U;
+    CNPUE = 0x0000U;
+    CNPDA = 0x0000U;
+    CNPDB = 0x0000U;
+    CNPDC = 0x0000U;
+    CNPDD = 0x0000U;
+    CNPDE = 0x0000U;
+
+
+    /****************************************************************************
+     * Setting the Open Drain SFR(s)
+     ***************************************************************************/
+    ODCA = 0x0000U;
+    ODCB = 0x0000U;
+    ODCC = 0x0000U;
+    ODCD = 0x0000U;
+    ODCE = 0x0000U;
+
+
+    /****************************************************************************
+     * Setting the Analog/Digital Configuration SFR(s)
+     ***************************************************************************/
+    ANSELA = 0x001FU;
+    ANSELB = 0x009FU;
+    ANSELC = 0x00CFU;
+    ANSELD = 0x2C00U;
+    ANSELE = 0x000FU;
+
+    /****************************************************************************
+     * Set the PPS
+     ***************************************************************************/
+     __builtin_write_RPCON(0x0000); // unlock PPS
+
+        RPINR18bits.U1RXR = 0x0042U; //RD2->UART1:U1RX;
+        RPOR17bits.RP67R = 0x0001U;  //RD3->UART1:U1TX;
+
+     __builtin_write_RPCON(0x0800); // lock PPS
+
+
+}
+
