0ddcef3f4d403bcca1f285c3bc0c5cdeb364af7c
C71887 <Gianni.Consiglio@microchip.com>
2026-01-29 15:07:36 -0700

    M16CRYPTO-204 updates to ML-KEM readme and copyright

--- dspic33ak512mps512/ml-kem/dspic33ak512mps512-pqc-ml-kem.X/crypto/test_vectors/ml_kem/decapsulate/ml_kem_1024_decapsulate.c
+++ dspic33ak512mps512/ml-kem/dspic33ak512mps512-pqc-ml-kem.X/crypto/test_vectors/ml_kem/decapsulate/ml_kem_1024_decapsulate.c
@@ -1,5 +1,5 @@
 /*
-? [2026] Microchip Technology Inc. and its subsidiaries.
+Copyright (c) [2026] Microchip Technology Inc. and its subsidiaries.
 
     Subject to your compliance with these terms, you may use Microchip
     software and any derivatives exclusively with Microchip products.
