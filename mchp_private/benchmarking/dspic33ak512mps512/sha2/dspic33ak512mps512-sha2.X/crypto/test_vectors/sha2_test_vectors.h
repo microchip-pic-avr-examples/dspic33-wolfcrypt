572df9fd08ca2702c293622d63a4ccb4de8d880d
C71887 <Gianni.Consiglio@microchip.com>
2026-02-05 15:29:23 -0700

    M16CRYPTO-206 Add dsPIC33AK SHA2 demo, create a benchmarking and readme for that demo

---
+++ mchp_private/benchmarking/dspic33ak512mps512/sha2/dspic33ak512mps512-sha2.X/crypto/test_vectors/sha2_test_vectors.h
@@ -0,0 +1,52 @@
+/*
+Copyright (C) [2026] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip
+    software and any derivatives exclusively with Microchip products.
+    You are responsible for complying with 3rd party license terms
+    applicable to your use of 3rd party software (including open source
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.?
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR
+    THIS SOFTWARE.
+*/
+
+#ifndef SHA2_TEST_VECTORS_H
+#define	SHA2_TEST_VECTORS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include "wolfssl/wolfcrypt/types.h"
+
+const TEST_VECTOR* SHA2_224_TestVectorsGet(void);
+
+word32 SHA2_224_VectorCountGet(void);
+
+const TEST_VECTOR* SHA2_256_TestVectorsGet(void);
+
+word32 SHA2_256_VectorCountGet(void);
+
+const TEST_VECTOR* SHA2_384_TestVectorsGet(void);
+
+word32 SHA2_384_VectorCountGet(void);
+
+const TEST_VECTOR* SHA2_512_TestVectorsGet(void);
+
+word32 SHA2_512_VectorCountGet(void);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SHA2_TEST_VECTORS_H */
+
