45d1b2bd6e13cbb4005b8946b06a76e3a5fe86d6
C71887 <Gianni.Consiglio@microchip.com>
2026-02-11 15:25:45 -0700

    M16CRYPTO-216 update user_settings based on feedback

--- dspic33ck256mp508/sha2/dspic33ck256mp508-sha2.X/app/user_settings.h
+++ dspic33ck256mp508/sha2/dspic33ck256mp508-sha2.X/app/user_settings.h
@@ -31,9 +31,9 @@ extern "C" {
     #define NO_SHA                      // Disable SHA related APIs
     #define NO_MD4                      // Disable MD4 related APIs
     #define NO_DSA                      // Disable DSA related APIs
-    #define NO_OLD_TLS                  // Disable TLS related APIs
+    #define NO_OLD_TLS                  // Disable settings for TLS protocol versions less than 1.2
     #define NO_ASN                      // Disable ASN related APIs
-    #define WC_NO_HARDEN                // Disable harden option
+    #define WC_NO_HARDEN                // Disables security countermeasures in wolfCrypt related to protecting against side-channel attacks
     #define NO_FILESYSTEM               // Specify device does not use a filesystem
     #define WOLFSSL_IGNORE_FILE_WARN    // Disable warning regarding inlined misc.c functions
 
@@ -45,10 +45,10 @@ extern "C" {
     // dsPIC33C Support
     #ifdef __dsPIC33C__
         #define WC_16BIT_CPU            // Specify 16-bit integer sizes
-        #define SIZEOF_LONG_LONG 4      // Define long value as 4 bytes
+        #define SIZEOF_LONG_LONG 4      // Define long long value size as 4 bytes
         #define SINGLE_THREADED         // Specify device is single threaded
         #define FAST_ROTATE             // Enable fast rotate operations
-        #define MICROCHIP_PIC24         // Enable Microchip PIC24 option for WORD64 requirements for 16bit devices
+        #define MICROCHIP_PIC24         // Enable Microchip PIC24 option to allow WORD64 support for 16bit devices
     #endif
 
 #ifdef	__cplusplus
